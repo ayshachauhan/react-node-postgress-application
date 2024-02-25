a2d5fd329c54ed35929971bebc4817321e44fc9f
RohitChilana <chilana.rohit@thinksys.com>
2024-02-25 13:00:28 +0530

    SUR-13,14: added docker compose for postgres and typeorm

--- services/backend/src/app.module.ts
+++ services/backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRootAsync({
+            useFactory: async () => {
+              return {
+                type: 'postgres',
+                host: process.env.DB_HOST || 'localhost',
+                port: Number(process.env.PORT) || 5432,
+                username: process.env.DB_USERNAME || 'postgres',
+                password: process.env.DB_PASSWORD || 'postgres',
+                database: process.env.DB_DATABASE || 'postgres',
+                autoLoadEntities: true,
+                synchronize: true,
+              };
+            },
+          }),
+        UsersModule,
+      ],
   controllers: [AppController],
   providers: [AppService],
 })
