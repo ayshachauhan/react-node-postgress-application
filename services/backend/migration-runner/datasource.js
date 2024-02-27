dab97510b929c16e58db43914cf47dd5ad03767b
RohitChilana <chilana.rohit@thinksys.com>
2024-02-27 19:00:43 +0530

    changes

--- services/backend/migration-runner/datasource.js
+++ services/backend/migration-runner/datasource.js
@@ -1,25 +1,17 @@
 const {DataSource} = require('typeorm')
 const path = require('path');
 
-
-// const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 const dbdatasource = {
-    // TypeORM PostgreSQL DB Drivers
     type: 'postgres',
     host: 'localhost',
     port: 5432,
     username: 'postgres',
     password: 'postgres',
-    // Database name
     database: 'postgres',
-    // entities: [path.join('__dirname', '..', 'src', 'entities', 'users.entity.js')],
-    migrations: [path.join('migrations', '*.ts')],
-    // Synchronize database schema with entities 
+    migrations: [path.join('migrations', '*.js')], 
     synchronize: false,
 };
-console.log(dbdatasource.migrations);
+
 const dataSource = new DataSource(dbdatasource);
 
 module.exports = dataSource;
-
