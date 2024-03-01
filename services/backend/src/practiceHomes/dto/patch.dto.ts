1a420492b1fe39ef4ccd83ca1bd51c9d69ba1bdc
Triyank <jain,triyank@thinksys.com>
2024-03-01 16:28:37 +0530

    endpoint changed as suggested

--- services/backend/src/practiceHomes/dto/patch.dto.ts
+++ services/backend/src/practiceHomes/dto/patch.dto.ts
@@ -1,10 +1,6 @@
-import { IsOptional, IsUUID } from 'class-validator';
+import { IsOptional } from 'class-validator';
 
 export class PracticeHomePatchDto {
-  @IsOptional()
-  @IsUUID('4')
-  practiceId: string;
-
   @IsOptional()
   name: string;
 }
