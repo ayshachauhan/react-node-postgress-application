01d11b6702b09ac75287b655259f9b6f54997bc4
anakshiant <nndchaudhary8@gmail.com>
2024-03-08 09:35:13 +0530

    user management apis

--- services/backend/src/auth/auth.guard.ts
+++ services/backend/src/auth/auth.guard.ts
@@ -23,6 +23,7 @@ export class AuthGuard implements CanActivate {
 
     try {
       const payload = await this.verify(token);
+      request['user'] = payload;
       return !payload?.isSuperAdmin;
     } catch (error) {
       console.log(error);
--- services/backend/src/auth/superAdmin.guard.ts
+++ services/backend/src/auth/superAdmin.guard.ts
@@ -20,6 +20,7 @@ export class SuperAdminGuard extends AuthGuard {
 
     try {
       const payload = await this.verify(token);
+      request['user'] = payload;
       return !!payload?.isSuperAdmin;
     } catch (error) {
       console.log(error);
