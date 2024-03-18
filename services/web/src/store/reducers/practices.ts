7c9f660d85bd60bad77dcfc59f14304889b56538
himani <aggarwal.himani@thinksys.com>
2024-03-19 02:08:24 +0530

    code review changes

--- services/web/src/store/reducers/practices.ts
+++ services/web/src/store/reducers/practices.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { PracticesInterface } from '@root/components/practices/types';
+import { State } from '@root/store';
 import { getPractices } from '../requests/practices';
 
 export interface PracticeState {
@@ -37,7 +38,11 @@ const practiceSlice = createSlice({
 
     builder.addCase(fetchListings.rejected, (state, action) => {
       state.status = 'failed';
-      state.error = action.payload ?? 'Failed to fetch practices';
+      if (typeof action.payload === 'string') {
+        state.error = action.payload ?? 'Failed to fetch practices';
+      } else {
+        state.error = 'Failed to fetch practices';
+      }
     });
   },
 });
@@ -47,8 +52,8 @@ export const fetchListings = createAsyncThunk(
   getPractices,
 );
 
-export const selectRecords = (state) => state.practices;
-export const selectStatus = (state) => state.practices.status;
-export const selectError = (state) => state.practices.error;
+export const selectRecords = (state: State) => state.practices;
+export const selectStatus = (state: State) => state.practices.status;
+export const selectError = (state: State) => state.practices.error;
 
 export default practiceSlice.reducer;
