016620a42b45122c19a2c185a08226e5a58821b2
anakshiant <nndchaudhary8@gmail.com>
2024-03-20 23:18:06 +0530

    fixed

--- services/web/src/app/(dashboard)/layout.tsx
+++ services/web/src/app/(dashboard)/layout.tsx
@@ -2,23 +2,15 @@
 import React, { useEffect } from 'react';
 
 import { SidebarLayout } from '@components/Layout';
-import { useAppDispatch, useAppSelector } from '@root/store';
-import {
-  fetchLoggedInUser,
-  selectIsAuthenticated,
-} from '@root/store/reducers/auth';
+import { useAppDispatch } from '@root/store';
+import { fetchLoggedInUser } from '@root/store/reducers/auth';
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const isAuthenticated = useAppSelector((state) =>
-    selectIsAuthenticated(state),
-  );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (isAuthenticated) {
-      dispatch(fetchLoggedInUser());
-    }
-  }, [isAuthenticated, dispatch]);
+    dispatch(fetchLoggedInUser());
+  }, [dispatch]);
 
   return (
     <>
