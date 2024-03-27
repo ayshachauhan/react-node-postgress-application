716cb3ba3b81342f8daf726f8b33945eccbd704e
himani <aggarwal.himani@thinksys.com>
2024-03-28 01:29:57 +0530

    allow only practice admin to access templates

--- services/web/src/components/Layout/SidebarLayout/Sidebar/types.ts
+++ services/web/src/components/Layout/SidebarLayout/Sidebar/types.ts
@@ -47,7 +47,7 @@ export const sidebarItems: SideBarItem[] = [
     id: 'templates',
     title: 'Templates',
     path: '/templates',
-    permissions: ['super_admin'],
+    permissions: ['admin'],
     Icon: TemplateIcon,
   },
   {
