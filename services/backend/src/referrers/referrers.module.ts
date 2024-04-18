63564024f66eefee0b5d498c35a90b68f3340587
triyank <jain.triyank@thinksys.com>
2024-04-18 19:02:45 +0530

    dependency resolved

--- services/backend/src/referrers/referrers.module.ts
+++ services/backend/src/referrers/referrers.module.ts
@@ -14,5 +14,6 @@ import { ReferrersService } from './referrers.service';
   ],
   controllers: [ReferrersController],
   providers: [ReferrersService, practiceNotFoundInterceptor],
+  exports: [ReferrersService],
 })
 export class ReferrersModule {}
