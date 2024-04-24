5e3fa56735a8ec77f87d4eda807b9c50785c2838
triyank <jain.triyank@thinksys.com>
2024-04-24 12:29:18 +0530

    extra files deleeted

--- services/backend/src/enums/evalEyeType.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum EVAL_EYE_TYPE {
-  Left = 'Left',
-  Right = 'Right',
-  Both = 'Both',
-}
--- services/backend/src/enums/evalStatus.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum EVAL_STATUS {
-  'Future Evaluation' = 'Future Evaluation',
-  Return = 'Return',
-  Cancel = 'Cancel',
-  'No Show' = 'No Show',
-}
