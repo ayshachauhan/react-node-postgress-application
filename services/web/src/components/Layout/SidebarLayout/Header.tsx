68895399f2b67e8543cea1b2c61f1a783885fb22
himani <aggarwal.himani@thinksys.com>
2024-05-07 20:40:01 +0530

    set first doctor as selected doctor on page load

--- services/web/src/components/Layout/SidebarLayout/Header.tsx
+++ services/web/src/components/Layout/SidebarLayout/Header.tsx
@@ -37,8 +37,8 @@ const Header: React.FC = () => {
     Object.values(entities).find((user) => user.id === userId);
 
   const [selectedUser, setSelectedUser] = useState<SanitizedUser | null>(null);
-  if (!localStorage.getItem(SELECTED_DOCTOR_KEY) && userInfo && userInfo.id) {
-    localStorage.setItem(SELECTED_DOCTOR_KEY, userInfo.id);
+  if (!localStorage.getItem(SELECTED_DOCTOR_KEY) && users.length > 0) {
+    localStorage.setItem(SELECTED_DOCTOR_KEY, users[0].id);
   }
 
   const is_super_admin = userInfo ? userInfo.isSuperAdmin : false;
