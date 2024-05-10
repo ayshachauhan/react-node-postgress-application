0f67a34c9fb0b9d453c487cb5c4b3c8fcadd7291
himani <aggarwal.himani@thinksys.com>
2024-05-10 17:04:49 +0530

    code review changes

--- services/web/src/components/Layout/SidebarLayout/Sidebar/Sidebar.tsx
+++ services/web/src/components/Layout/SidebarLayout/Sidebar/Sidebar.tsx
@@ -9,6 +9,8 @@ import { SideBarItem, filterSidebarItems, sidebarItems } from './types';
 
 const Sidebar: React.FC = () => {
   const [activeMenuItemId, setActiveMenuItemId] = useState<string>('');
+  const [activeChildMenuItemId, setActiveChildMenuItemId] =
+    useState<string>('');
   const userInfo = useAppSelector((state) => state.auth.user);
   const is_super_admin = userInfo ? userInfo.isSuperAdmin : false;
   const userType = is_super_admin ? 'super_admin' : 'admin';
@@ -19,6 +21,10 @@ const Sidebar: React.FC = () => {
 
   function handleSidebarItemClick(item: SideBarItem) {
     setActiveMenuItemId(item.id);
+  }
+
+  function handleSidebarChildItemClick(item) {
+    setActiveChildMenuItemId(item.id);
   }
 
   useEffect(() => {
@@ -68,10 +74,9 @@ const Sidebar: React.FC = () => {
               {item.child && (
                 <ul
                   className={clsx(
-                    'ease-in-out duration-300 my-1 py-2 space-y-2 rounded-lg',
+                    'ease-in-out duration-300 my-1 ml-5 py-2 space-y-2 rounded-lg',
                     {
                       hidden: item.id !== activeMenuItemId,
-                      'bg-secondary': item.id === activeMenuItemId,
                     },
                   )}
                 >
@@ -79,7 +84,14 @@ const Sidebar: React.FC = () => {
                     <li key={childItem.id}>
                       <Link
                         href={childItem.path}
-                        className="flex items-center w-full p-1 rounded-lg pl-5 group text-white text-xs font-normal"
+                        onClick={() => handleSidebarChildItemClick(childItem)}
+                        className={clsx(
+                          'flex items-center w-full p-1 rounded-lg group text-white text-xs font-normal',
+                          {
+                            'bg-secondary':
+                              childItem.id === activeChildMenuItemId,
+                          },
+                        )}
                       >
                         <span className="mr-2">
                           <ChevronRightSmall />
