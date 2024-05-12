b2388f0d5950b843016a2637caf4732c862fa085
anakshiant <nndchaudhary8@gmail.com>
2024-05-12 19:54:38 +0530

    changed db server name

--- terraform/shared/rds.tf
+++ terraform/shared/rds.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "main" {
   apply_immediately    = true
   engine               = "postgres"
   engine_version       = "16.1"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t4g.micro"
   storage_type         = "gp2"
 
   allocated_storage            = 20
