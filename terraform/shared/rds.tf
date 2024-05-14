866218dac4de4beee59a74eb68fa24518db769b5
anakshiant <nndchaudhary8@gmail.com>
2024-05-14 13:57:42 +0530

    pg group fix

--- terraform/shared/rds.tf
+++ terraform/shared/rds.tf
@@ -40,6 +40,11 @@ resource "aws_db_parameter_group" "postgres16" {
         value        = "0"
         apply_method = "pending-reboot"
       },
+       {
+        name         = "ssl"
+        value        = "0"
+        apply_method = "pending-reboot"
+      },
     ]
     content {
       apply_method = lookup(parameter.value, "apply_method", null)
