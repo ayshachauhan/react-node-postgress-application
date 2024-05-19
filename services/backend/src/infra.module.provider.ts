57c75daf7531271a807bf428051794137fd9f45a
abhishek <abhisheksharma2013011@gmail.com>
2024-05-19 17:20:03 +0530

    ran formatter

--- services/backend/src/infra.module.provider.ts
+++ services/backend/src/infra.module.provider.ts
@@ -93,7 +93,7 @@ export const createInfraModuleProviders = (): Array<
           PatientEntity,
           EvalEntity,
           SurgeryConfigurationEntity,
-          HistoryEntity
+          HistoryEntity,
         ],
         synchronize: false,
       }),
