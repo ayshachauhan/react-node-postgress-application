c04283b12d0987cbde63af91d2497ab1fe950e1f
Nirmal Garg <mcl-0082@Nirmals-MacBook-Pro.local>
2024-05-29 21:34:33 +0530

    test-5

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -4,7 +4,7 @@ module "shared" {
   environment           = var.environment
   environment_variables = var.environment_variables
   availability_zones    = [var.availability_zones[0], var.availability_zones[1]]
-  subnet_ids            = [var.subnet_ids[0], var.subnet_ids[1]]
+  subnet_ids            = [module.shared.private_subnet_ids[0], module.shared.private_subnet_ids[1]]
 }
 
 
