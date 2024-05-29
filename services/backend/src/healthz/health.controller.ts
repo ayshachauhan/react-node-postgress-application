b9c69734fda21f26216d5e2135834677963efb05
anakshiant <nndchaudhary8@gmail.com>
2024-05-29 20:49:10 +0530

    removedc commented code

--- services/backend/src/healthz/health.controller.ts
+++ services/backend/src/healthz/health.controller.ts
@@ -13,38 +13,4 @@ export class HealthController {
   async check() {
     return this.healthService.check();
   }
-
-  // @Post('upload')
-  // @UseInterceptors(FileInterceptor('file'))
-  // async uploadFile(
-  //   @UploadedFile(
-  //     new ParseFilePipe({
-  //       validators: [
-  //         new MaxFileSizeValidator({ maxSize: 1000000 }),
-  //         new FileTypeValidator({ fileType: 'image/*' }),
-  //       ],
-  //     }),
-  //   )
-  //   file,
-  // ) {
-  //   console.log(file);
-  //   const x = await s3
-  //     .upload({
-  //       Bucket: 'azentia-qa',
-  //       Key: file.originalname,
-  //       Body: file.buffer,
-  //       ACL: 'public-read',
-  //       ContentType: file.mimetype,
-  //     })
-  //     .promise();
-
-  //   console.log(x, 'uploadf');
-  //   const listObjectsResult = await s3
-  //     .listObjectsV2({ Bucket: 'azentia-qa' }) // Replace 'qa' with your bucket name
-  //     .promise();
-
-  //   console.log(listObjectsResult, 'listObjectsResult');
-
-  //   return 'ok';
-  // }
 }
