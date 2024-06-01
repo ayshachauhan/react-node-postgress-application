ec82897fabcee06a53732c316489e4c1bbd96d8b
abhishek <abhisheksharma2013011@gmail.com>
2024-06-01 18:03:36 +0530

    updated media entity

--- packages/entities/src/media/media.entity.ts
+++ packages/entities/src/media/media.entity.ts
@@ -1,23 +1,19 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from '../base.entity';
-import { SurgeryConfigurationEntity } from '../surgeryConfiguration';
-import { IMedia } from './media.interface';
+import { IMedia, MediaConfig, MediaType } from './media.interface';
 
 @Entity('videos')
 export class VideoEntity extends BaseEntity implements IMedia {
   @Column({ type: 'uuid' })
   practiceId: string;
 
-  @Column({ type: 'varchar' })
-  name: string;
+  @Column({
+    type: 'enum',
+    enum: MediaType,
+    nullable: true,
+  })
+  mediaType: MediaType;
 
-  @Column({ type: 'varchar' })
-  urlEmbed: string;
-
-  @Column({ type: 'varchar' })
-  url: string;
-
-  @ManyToOne(() => SurgeryConfigurationEntity)
-  @JoinColumn({ name: 'surgeryConfigurationId' })
-  surgeryConfiguration: SurgeryConfigurationEntity;
+  @Column({ type: 'jsonb' })
+  mediaConfig: MediaConfig;
 }
--- packages/entities/src/media/media.interface.ts
+++ packages/entities/src/media/media.interface.ts
@@ -2,11 +2,9 @@ import { IBaseEntity } from '../base.interface';
 import { ISurgeryConfiguration } from '../surgeryConfiguration';
 
 export interface IMedia extends IBaseEntity {
-  name: string;
-  urlEmbed: string;
-  url: string;
   practiceId: string;
-  surgeryConfiguration: ISurgeryConfiguration;
+  mediaType: MediaType;
+  mediaConfig: MediaConfig;
 }
 
 export type IMediaRequest = {
@@ -15,4 +13,32 @@ export type IMediaRequest = {
   url: string;
   practiceId: string;
   surgeryConfigurationId: string;
+};
+
+export enum MediaType {
+  PRACTICE = 'practice',
+  PATIENT = 'patient',
+}
+
+export type MediaConfig = PracticeMediaConfig | PatientMediaConfig;
+
+export type Video = {
+  title: string;
+  url: string;
+};
+
+export type Image = {
+  title: string;
+  url: string;
+};
+
+export type PracticeMediaConfig = {
+  surgeryConfiguration: ISurgeryConfiguration;
+  video: Video[];
+};
+
+export type PatientMediaConfig = {
+  mrn: number;
+  video: Video[];
+  image: Image[];
 };
