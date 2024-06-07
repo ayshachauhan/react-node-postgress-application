0a3ae325376a557e63241cf18276d99f655e17bb
triyank <jain.triyank@thinksys.com>
2024-06-07 11:58:48 +0530

    znt-128 fixed . duplicate email while adding a practice to adninb

--- services/backend/src/users/users.controller.ts
+++ services/backend/src/users/users.controller.ts
@@ -35,7 +35,12 @@ export class UsersController {
     @Body(new ValidationPipe()) createUserDto: CreateUserDto,
     @Param() { practiceId }: { practiceId: string },
   ): Promise<SanitizedUser> {
-    return this.usersService.create(createUserDto, practiceId);
+    const createFromUserController: boolean = true;
+    return this.usersService.create(
+      createUserDto,
+      practiceId,
+      createFromUserController,
+    );
   }
 
   @Get()
