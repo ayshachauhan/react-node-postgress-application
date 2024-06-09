3a79bbebafde304ab74508e19d25df33e8099b4b
anakshiant <nndchaudhary8@gmail.com>
2024-06-09 17:51:11 +0530

    added attachment to templates

--- services/web/src/app/(dashboard)/profile/page.tsx
+++ services/web/src/app/(dashboard)/profile/page.tsx
@@ -2,7 +2,6 @@
 import Button from '@root/components/Button';
 import { AvatarIcon } from '@root/components/Icons';
 import { useAppDispatch, useAppSelector } from '@root/store';
-import { fetchLoggedInUser } from '@root/store/reducers/auth';
 import { getPracticeInfo } from '@root/store/reducers/practices';
 import { fetchListings as fetchPermissions } from '@root/store/reducers/userPermissions';
 import { getPracticeId } from '@utils/index';
@@ -33,10 +32,6 @@ const Profile: React.FC = () => {
       dispatch(getPracticeInfo({ id: userPracticeId }));
     }
   }, [userPracticeId, dispatch]);
-
-  useEffect(() => {
-    dispatch(fetchLoggedInUser());
-  }, [dispatch]);
 
   useEffect(() => {
     dispatch(fetchPermissions(undefined));
