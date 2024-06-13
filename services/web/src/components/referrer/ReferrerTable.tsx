1259fe338bf4b30688959421360e3c13d805523b
triyank <jain.triyank@thinksys.com>
2024-06-13 14:46:19 +0530

    referrer table nan view fixed

--- services/web/src/components/referrer/ReferrerTable.tsx
+++ services/web/src/components/referrer/ReferrerTable.tsx
@@ -192,7 +192,7 @@ export default function ReferrerTable() {
                   {data?.referrerType}
                 </div>
                 <div className="text-gray-900 bg-gray-50 pt-2 px-4 flex-1">
-                  {total ?? 0}
+                  {total ? total : 0}
                 </div>
                 <div className="text-gray-900 bg-gray-50 pt-2 px-4 flex-1">
                   {data?.email}
