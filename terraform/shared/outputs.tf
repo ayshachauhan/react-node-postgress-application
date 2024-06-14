7fb720c9407b36ccb2884b57efc4bfb7d4646d6e
anakshiant <nndchaudhary8@gmail.com>
2024-06-14 20:55:13 +0530

    trigger ci

--- terraform/shared/outputs.tf
+++ terraform/shared/outputs.tf
@@ -16,6 +16,13 @@ output "service_security_group_id" {
   value = aws_security_group.service_security_group.id
 }
 
+output "aws_security_group" {
+  value = {
+    postgres = aws_security_group.postgres.id
+  }
+}
+
+
 output "load_balancer_security_group_id" {
   value = aws_security_group.load_balancer_security_group.id
 }
