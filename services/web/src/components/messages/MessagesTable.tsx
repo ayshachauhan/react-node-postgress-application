ba9ac254600b35e0f8fffb61db446951e84c3dd5
himani <aggarwal.himani@thinksys.com>
2024-06-24 15:36:42 +0530

    fix

--- services/web/src/components/messages/MessagesTable.tsx
+++ services/web/src/components/messages/MessagesTable.tsx
@@ -449,122 +449,126 @@ export default function MessagesTable() {
         </div>
       ) : null}
       <hr className="h-px my-1.5 bg-gray-100 border-1 dark:bg-gray-700"></hr>
-      <div className="flex w-full bg-green-50 pr-2 border-b border-green-200 items-center">
-        <div className="flex items-center">
-          {['All', 'Emails', 'Texts', 'Referrers'].map((item) => (
-            <div className="mr-1" key={item}>
-              <button
-                className="py-2 px-4 text-xs text-black text-normal border-b-2 border-transparent hover:text-white hover:bg-gradient-to-r from-primary-light to-primary-dark hover:rounded-t-lg"
-                style={{
-                  ...(activeButton === item && {
-                    backgroundImage:
-                      'linear-gradient(to right, rgba(53, 165, 118, 1), rgba(17, 113, 128, 1))',
-                    color: 'white',
-                    borderTopLeftRadius: '0.5rem',
-                    borderTopRightRadius: '0.5rem',
-                  }),
-                }}
-                onClick={() => toggleActive(item)}
-              >
-                {item}
-              </button>
-            </div>
-          ))}
-        </div>
-      </div>
-      {Object.keys(sortedMessagesByDate).length !== 0 && (
-        <div className="w-full overflow-x-auto mt-2 border rounded-t-lg rounded-b-lg border-gray-200">
-          {Object.entries(sortedMessagesByDate).length > 0 &&
-            Object.entries(sortedMessagesByDate).map(
-              ([date, records], index) => (
-                <div key={date}>
-                  <div
-                    className={`border-solid px-2.5 py-3 text-white text-base font-normal ${
-                      index == 0 ? 'rounded-t-lg' : ''
-                    }`}
-                    style={{ backgroundColor: 'rgba(53, 165, 118, 1)' }}
+      {!isLoading && (
+        <div>
+          <div className="flex w-full bg-green-50 pr-2 border-b border-green-200 items-center">
+            <div className="flex items-center">
+              {['All', 'Emails', 'Texts', 'Referrers'].map((item) => (
+                <div className="mr-1" key={item}>
+                  <button
+                    className="py-2 px-4 text-xs text-black text-normal border-b-2 border-transparent hover:text-white hover:bg-gradient-to-r from-primary-light to-primary-dark hover:rounded-t-lg"
+                    style={{
+                      ...(activeButton === item && {
+                        backgroundImage:
+                          'linear-gradient(to right, rgba(53, 165, 118, 1), rgba(17, 113, 128, 1))',
+                        color: 'white',
+                        borderTopLeftRadius: '0.5rem',
+                        borderTopRightRadius: '0.5rem',
+                      }),
+                    }}
+                    onClick={() => toggleActive(item)}
                   >
-                    {formatHeaderDate(date)}
-                  </div>
-                  <div className="bg-gradient-to-br from-teal-600 to-green-500  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 flex justify-between py-2 px-2.5 text-sm">
-                    <div className="font-bold text-white py-2 px-1 min-w-[10rem]">
-                      Date | Time
-                    </div>
-                    <div className="font-bold text-white py-2 px-1 min-w-[10rem]">
-                      Surgery
-                    </div>
-                    <div className="font-bold text-white py-2 px-1 min-w-[10rem]">
-                      Patient
-                    </div>
-                    <div className="font-bold text-white py-2 px-1 min-w-[5rem]">
-                      MRN
-                    </div>
-                    <div className="font-bold text-white py-2 px-1 min-w-[10rem]">
-                      Contact Details
-                    </div>
-                    {activeButton !== 'Texts' && (
-                      <div className="font-bold text-white py-2 px-1 min-w-[20rem] max-w-[20rem]">
-                        Email
-                      </div>
-                    )}
-                    {activeButton !== 'Emails' && (
-                      <div className="font-bold text-white py-2 px-1 min-w-[20rem] max-w-[20rem]">
-                        Text
-                      </div>
-                    )}
-                  </div>
-                  {records.map((row, index) => (
-                    <div
-                      key={row.id}
-                      id={row.id}
-                      className={`div-clone flex justify-between px-2.5 text-xs ${
-                        index !== records.length - 1
-                          ? 'border-b border-gray-300'
-                          : ''
-                      }`}
-                    >
-                      <div className="text-black  pt-2 pb-2 px-1 min-w-[10rem]">
-                        {formatColumnDate(row.dateCreated)}
-                      </div>
-                      <div className="text-black  pt-2 pb-2 px-1 min-w-[10rem]">
-                        {row?.data?.Laterality} {row?.data?.surgery_type}
-                      </div>
-                      <div className="text-black  pt-2 pb-2 px-1 min-w-[10rem]">
-                        {row
-                          ? generateFullName(
-                              row?.data?.fname ?? '',
-                              row?.data?.lname ?? '',
-                            )
-                          : null}
-                      </div>
-                      <div className="text-black  pt-2 pb-2 px-1 min-w-[5rem]">
-                        {row?.data?.mrn}
-                      </div>
-                      <div className="text-black  pt-2 pb-2 px-1 overflow-hidden min-w-[10rem]">
-                        <p>Cell: {row?.data?.phoneNumber}</p>
-                        <p>Email: {row?.data?.pt_email_address}</p>
-                      </div>
-                      {activeButton !== 'Texts' && (
-                        <MessageWithReadMore
-                          message={convertVariables(
-                            row?.data?.body ?? '',
-                            row?.data,
-                          )}
-                        />
-                      )}
-                      {activeButton !== 'Emails' && (
-                        <MessageWithReadMore
-                          message={convertVariables(
-                            row?.data?.text ?? '',
-                            row?.data,
-                          )}
-                        />
-                      )}
-                    </div>
-                  ))}
+                    {item}
+                  </button>
                 </div>
-              ),
-            )}
+              ))}
+            </div>
+          </div>
+          {Object.keys(sortedMessagesByDate).length !== 0 && (
+            <div className="w-full overflow-x-auto mt-2 border rounded-t-lg rounded-b-lg border-gray-200">
+              {Object.entries(sortedMessagesByDate).length > 0 &&
+                Object.entries(sortedMessagesByDate).map(
+                  ([date, records], index) => (
+                    <div key={date}>
+                      <div
+                        className={`border-solid px-2.5 py-3 text-white text-base font-normal ${
+                          index == 0 ? 'rounded-t-lg' : ''
+                        }`}
+                        style={{ backgroundColor: 'rgba(53, 165, 118, 1)' }}
+                      >
+                        {formatHeaderDate(date)}
+                      </div>
+                      <div className="bg-gradient-to-br from-teal-600 to-green-500  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 flex justify-between py-2 px-2.5 text-sm">
+                        <div className="font-bold text-white py-2 px-1 min-w-[10rem]">
+                          Date | Time
+                        </div>
+                        <div className="font-bold text-white py-2 px-1 min-w-[10rem]">
+                          Surgery
+                        </div>
+                        <div className="font-bold text-white py-2 px-1 min-w-[10rem]">
+                          Patient
+                        </div>
+                        <div className="font-bold text-white py-2 px-1 min-w-[5rem]">
+                          MRN
+                        </div>
+                        <div className="font-bold text-white py-2 px-1 min-w-[10rem]">
+                          Contact Details
+                        </div>
+                        {activeButton !== 'Texts' && (
+                          <div className="font-bold text-white py-2 px-1 min-w-[20rem] max-w-[20rem]">
+                            Email
+                          </div>
+                        )}
+                        {activeButton !== 'Emails' && (
+                          <div className="font-bold text-white py-2 px-1 min-w-[20rem] max-w-[20rem]">
+                            Text
+                          </div>
+                        )}
+                      </div>
+                      {records.map((row, index) => (
+                        <div
+                          key={row.id}
+                          id={row.id}
+                          className={`div-clone flex justify-between px-2.5 text-xs ${
+                            index !== records.length - 1
+                              ? 'border-b border-gray-300'
+                              : ''
+                          }`}
+                        >
+                          <div className="text-black  pt-2 pb-2 px-1 min-w-[10rem]">
+                            {formatColumnDate(row.dateCreated)}
+                          </div>
+                          <div className="text-black  pt-2 pb-2 px-1 min-w-[10rem]">
+                            {row?.data?.Laterality} {row?.data?.surgery_type}
+                          </div>
+                          <div className="text-black  pt-2 pb-2 px-1 min-w-[10rem]">
+                            {row
+                              ? generateFullName(
+                                  row?.data?.fname ?? '',
+                                  row?.data?.lname ?? '',
+                                )
+                              : null}
+                          </div>
+                          <div className="text-black  pt-2 pb-2 px-1 min-w-[5rem]">
+                            {row?.data?.mrn}
+                          </div>
+                          <div className="text-black  pt-2 pb-2 px-1 overflow-hidden min-w-[10rem]">
+                            <p>Cell: {row?.data?.phoneNumber}</p>
+                            <p>Email: {row?.data?.pt_email_address}</p>
+                          </div>
+                          {activeButton !== 'Texts' && (
+                            <MessageWithReadMore
+                              message={convertVariables(
+                                row?.data?.body ?? '',
+                                row?.data,
+                              )}
+                            />
+                          )}
+                          {activeButton !== 'Emails' && (
+                            <MessageWithReadMore
+                              message={convertVariables(
+                                row?.data?.text ?? '',
+                                row?.data,
+                              )}
+                            />
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                  ),
+                )}
+            </div>
+          )}
         </div>
       )}
     </div>
