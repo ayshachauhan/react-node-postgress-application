9eecf97ff3736ad67e17aedb992af7868a6d2771
Avneesh Kumar <kumar.avneesh@thinksys.com>
2024-07-03 10:11:09 +0530

    minor change

--- services/web/src/components/messages/MessagesTable.tsx
+++ services/web/src/components/messages/MessagesTable.tsx
@@ -170,6 +170,7 @@ export default function MessagesTable() {
   const resetFilters = (): void => {
     dispatch(setSearchMRNName(null));
     setMrn('');
+    setSendVideoDisable(true);
   };
 
   const handleSearchMRNNameChange = (value) => {
