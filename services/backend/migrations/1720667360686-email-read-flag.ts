29e14e1532f6884ca2e32346474bae770b0c79d1
Triyank <jain.triyank@thinksys.com>
2024-07-11 08:43:20 +0530

    migration for email read functionality

---
+++ services/backend/migrations/1720667360686-email-read-flag.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class EmailReadFlag1720667360686 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'email_logs',
+      new TableColumn({
+        name: 'isRead',
+        type: 'boolean',
+        isNullable: false,
+        default: false,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
