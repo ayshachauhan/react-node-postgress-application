549825f28a616ef31e7d34fd21d1d2afd5e27a53
himani <aggarwal.himani@thinksys.com>
2024-08-02 10:31:18 +0530

    fix

--- services/web/src/context/UserPermissionsContext.js
+++ services/web/src/context/UserPermissionsContext.js
@@ -1,28 +1,10 @@
 'use client';
-import { useAppDispatch, useAppSelector } from '@root/store';
-import { fetchLoggedInUser } from '@root/store/reducers/auth';
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const UserPermissionsContext = createContext();
 
 export const UserPermissionsProvider = ({ children }) => {
-  const dispatch = useAppDispatch();
-  const userInfo = useAppSelector((state) => state.auth);
-  const [userPermissions, setUserPermissions] = useState(
-    userInfo?.permissions || [],
-  );
-
-  useEffect(() => {
-    dispatch(fetchLoggedInUser());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (userInfo) {
-      setUserPermissions(userInfo.permissions || []);
-    } else {
-      setUserPermissions([]);
-    }
-  }, [userInfo]);
+  const [userPermissions, setUserPermissions] = useState([]);
 
   const updateUserPermissions = (newPermissions) => {
     setUserPermissions(newPermissions);
