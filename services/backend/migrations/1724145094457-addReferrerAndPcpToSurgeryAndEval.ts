c5e25942820496fc90adaef447201cd45920be86
Avneesh kumar <kumar.avneesh@thinksys.com>
2024-09-05 11:18:16 +0530

    script corrected

--- services/backend/migrations/1724145094457-addReferrerAndPcpToSurgeryAndEval.ts
+++ services/backend/migrations/1724145094457-addReferrerAndPcpToSurgeryAndEval.ts
@@ -5,10 +5,8 @@ export class AddReferrerAndPcpToSurgeryAndEval1724145094457
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE public.surgeries ADD COLUMN "referrerId" TYPE uuid USING NULLIF("referrerId", '')::uuid, 
-            ALTER TABLE public.evals ADD COLUMN "referrerId" TYPE uuid USING NULLIF("referrerId", '')::uuid, 
-            ALTER TABLE public.surgeries ADD COLUMN "pcp" TYPE uuid USING NULLIF("pcp", '')::uuid, 
-            ALTER TABLE public.evals ADD COLUMN "pcp" TYPE uuid USING NULLIF("pcp", '')::uuid`,
+      `ALTER TABLE public.surgeries ADD COLUMN "referrerId" uuid, ADD COLUMN "pcp" uuid;
+      ALTER TABLE public.evals ADD COLUMN "referrerId" uuid, ADD COLUMN "pcp" uuid;`,
     );
   }
 
