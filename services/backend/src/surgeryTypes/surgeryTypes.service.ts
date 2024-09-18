efed9e6708b4905e78bfe6f79f3bfe6550632b15
himani <aggarwal.himani@thinksys.com>
2024-09-18 21:58:10 +0530

    order by datecreated desc

--- services/backend/src/surgeryTypes/surgeryTypes.service.ts
+++ services/backend/src/surgeryTypes/surgeryTypes.service.ts
@@ -28,6 +28,7 @@ export class SurgeryTypesService {
   ): Promise<SurgeryTypeEntity | null> {
     return this.surgeryTypeRepository.findOne({
       where: { id, practice: { id: practiceId } },
+      order: { dateCreated: 'DESC' },
     });
   }
 
