38f95d412d75335bf0d56b0ec339a57088cdb906
himani <aggarwal.himani@thinksys.com>
2025-02-16 02:28:33 +0530

    fix vulnerability testing

--- services/backend/src/surgeryTypes/surgeryTypes.module.ts
+++ services/backend/src/surgeryTypes/surgeryTypes.module.ts
@@ -1,18 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SurgeryTypeEntity } from '@packages/entities/surgeryType';
-import { CalendarModule } from 'src/calendar/calendar.module';
-import { EvalsModule } from 'src/evals/evals.module';
-import { HistoryModule } from 'src/history/history.module';
-import { InsuranceTypesModule } from 'src/insuranceTypes/insuranceTypes.module';
-import { MediaModule } from 'src/media/media.module';
-import { PracticeHomesModule } from 'src/practiceHomes/practiceHomes.module';
-import { ReferrersModule } from 'src/referrers/referrers.module';
-import { SurgeryModule } from 'src/surgery/surgery.module';
-import { SurgeryConfigurationsModule } from 'src/surgeryConfiguration/surgeryConfiguration.module';
-import { TemplatesModule } from 'src/templates/templates.module';
 import { UsersModule } from 'src/users/users.module';
-import { WaitlistModule } from 'src/waitlist/waitlist.module';
 import { PracticesModule } from '../practices/practices.module';
 import { SurgeryTypesController } from './surgeryType.controller';
 import { SurgeryTypesService } from './surgeryTypes.service';
@@ -22,17 +11,6 @@ import { SurgeryTypesService } from './surgeryTypes.service';
     TypeOrmModule.forFeature([SurgeryTypeEntity]),
     forwardRef(() => PracticesModule),
     forwardRef(() => UsersModule),
-    forwardRef(() => EvalsModule),
-    forwardRef(() => SurgeryModule),
-    forwardRef(() => CalendarModule),
-    forwardRef(() => TemplatesModule),
-    forwardRef(() => HistoryModule),
-    forwardRef(() => MediaModule),
-    forwardRef(() => PracticeHomesModule),
-    forwardRef(() => ReferrersModule),
-    forwardRef(() => WaitlistModule),
-    forwardRef(() => InsuranceTypesModule),
-    forwardRef(() => SurgeryConfigurationsModule),
   ],
   providers: [SurgeryTypesService],
   controllers: [SurgeryTypesController],
