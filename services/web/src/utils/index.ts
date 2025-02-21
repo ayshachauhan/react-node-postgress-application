d43d7f711ed18c0eba62177ce4a83656ff9bc29b
himani <aggarwal.himani@thinksys.com>
2025-02-21 13:55:22 +0530

    uncomment crypto related code

--- services/web/src/utils/index.ts
+++ services/web/src/utils/index.ts
@@ -1,10 +1,10 @@
 import { SurgeryStatus } from '@packages/entities';
 import { ICalendar, MonthOption } from '@packages/entities/index.browser';
 import { DEFAULT_SURGERYLOCATION_COLOR } from '@root/utils/constants';
-// import CryptoJS from 'crypto-js';
+import CryptoJS from 'crypto-js';
 import moment from 'moment';
 
-// const secretKey = process.env.NEXT_PUBLIC_ENCRYPTION_KEY;
+const secretKey = process.env.NEXT_PUBLIC_ENCRYPTION_KEY;
 
 export function indexBy<K extends keyof T, T>(
   key: K,
@@ -450,6 +450,6 @@ export const validatePassword = (password: string): string | null => {
   return null; // No error if it matches the pattern
 };
 
-// export function encryptPassword(password: string): string {
-//   return CryptoJS.AES.encrypt(password, secretKey).toString();
-// }
+export function encryptPassword(password: string): string {
+  return CryptoJS.AES.encrypt(password, secretKey).toString();
+}
