775738c624830488b2cf421ff69bab72bb95e797
himani <aggarwal.himani@thinksys.com>
2025-03-30 15:52:50 +0530

    remove password and token from users list, calendar

--- services/backend/src/calendar/calendar.service.ts
+++ services/backend/src/calendar/calendar.service.ts
@@ -86,7 +86,7 @@ export class CalendarService {
     practiceId,
     userId,
   }: GetCalendarsParams): Promise<CalendarEntity[]> {
-    return await this.calendarRepo.find({
+    const calendars = await this.calendarRepo.find({
       where: {
         practice: { id: practiceId },
         user: {
@@ -94,6 +94,19 @@ export class CalendarService {
         },
       },
       relations: ['practice', 'surgeryType', 'user'],
+    });
+
+    return calendars.map((calendar) => {
+      const { password, token, ...userWithoutPassword } = calendar.user;
+      password && password;
+      token && token;
+
+      return {
+        ...calendar,
+        user: {
+          ...userWithoutPassword,
+        } as UserEntity,
+      };
     });
   }
 
@@ -112,7 +125,17 @@ export class CalendarService {
     if (!response) {
       throw new NotFoundException('Calendar does not exists');
     }
-    return response;
+
+    const { password, token, ...userWithoutPassword } = response.user;
+    password && password;
+    token && token;
+
+    return {
+      ...response,
+      user: {
+        ...userWithoutPassword,
+      } as UserEntity,
+    };
   }
 
   /**
@@ -136,7 +159,18 @@ export class CalendarService {
         'Calendar does not exists for this surgerytype',
       );
     }
-    return response;
+    return response.map((calendar) => {
+      const { password, token, ...userWithoutPassword } = calendar.user;
+      password && password;
+      token && token;
+
+      return {
+        ...calendar,
+        user: {
+          ...userWithoutPassword,
+        } as UserEntity,
+      };
+    });
   }
 
   /**
@@ -333,6 +367,14 @@ export class CalendarService {
           relations: ['practice', 'surgeryType', 'user'],
         })) as CalendarEntity;
 
+        if (updatedCalendar?.user) {
+          const { password, token, ...userWithoutPassword } =
+            updatedCalendar.user; //remove password and token from response
+          password && password;
+          token && token;
+          updatedCalendar.user = userWithoutPassword as UserEntity;
+        }
+
         updatedCalendars.push(updatedCalendar);
       }),
     );
@@ -499,10 +541,28 @@ export class CalendarService {
         ) &&
           dbCalendarsWithoutPermission.some((s) => s.date > new Date())));
 
+    const sanitizeCalendars = (
+      calendars: CalendarEntity[],
+    ): CalendarEntity[] => {
+      return calendars.map((calendar) => {
+        const { password, token, ...userWithoutPassword } = calendar.user;
+        password && password;
+        token && token;
+        return {
+          ...calendar,
+          user: userWithoutPassword as UserEntity,
+        };
+      });
+    };
+    const sanitizedCalendars = sanitizeCalendars(dbCalendars);
+    const sanitizedCalendarsWithoutPermission = sanitizeCalendars(
+      dbCalendarsWithoutPermission,
+    );
+
     return {
-      calendars: dbCalendars,
+      calendars: sanitizedCalendars,
       restricted,
-      calendarsWithoutPermission: dbCalendarsWithoutPermission,
+      calendarsWithoutPermission: sanitizedCalendarsWithoutPermission,
     };
   }
 }
