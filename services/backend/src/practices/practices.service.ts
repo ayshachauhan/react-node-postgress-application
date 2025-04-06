a637fad0c7bdc53d141477d3d9a75f1d19a4606c
himaniaggarwal01 <aggarwal.himani@thinksys.com>
2025-04-06 17:40:17 +0530

    fix user for,

--- services/backend/src/practices/practices.service.ts
+++ services/backend/src/practices/practices.service.ts
@@ -182,7 +182,7 @@ export class PracticesService {
           userName: `${adminEmail}`,
           type: UserType.ADMIN,
           url: '',
-          designation: '',
+          designation: undefined,
           contactNumber: adminContactNumber,
           countryCode: adminCountryCode,
           permissionIds: [],
