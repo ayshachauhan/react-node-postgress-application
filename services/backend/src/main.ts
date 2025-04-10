0ca0d3ae619d72996c5f90bbaed44df042ad1d94
himaniaggarwal01 <aggarwal.himani@thinksys.com>
2025-04-10 12:44:23 +0530

    add missing headers

--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:8081'],
+    origin: ['https://app-qa.pod111.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
