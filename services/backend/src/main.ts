ff33888a776caf84c34a0fd378cfcb8af862f3e4
himaniaggarwal01 <aggarwal.himani@thinksys.com>
2025-04-15 12:39:37 +0530

    remove unwanted emthods

--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [frontendBaseUrl],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
   });
 
